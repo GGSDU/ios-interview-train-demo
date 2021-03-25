58799a60f295dfdbab0f960b561dc7250c96fe1d
taochunjie <337995545@qq.com>
2021-03-25 17:11:53 +0800

    常驻线程

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -8,6 +8,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) NSThread *thread;
 
 @end
 
@@ -18,7 +20,9 @@
     // Do any additional setup after loading the view.
     
 //    [self showDemo1];
-    [self showDemo2];
+//    [self showDemo2];
+//    [self showDemo3];
+    [self showDemo4];
 }
 
 - (void)showDemo1 {
@@ -41,5 +45,51 @@
 //    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
 }
 
+- (IBAction)btnClick:(id)sender {
+    NSLog(@"btnClick...");
+}
+
+- (void)showDemo3 {
+    // 创建观察者
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreateWithHandler(CFAllocatorGetDefault(), kCFRunLoopAllActivities, YES, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+        NSLog(@"监听到RunLoop发生改变---%zd",activity);
+    });
+
+    // 添加观察者到当前RunLoop中
+    CFRunLoopAddObserver(CFRunLoopGetCurrent(), observer, kCFRunLoopDefaultMode);
+
+    // 释放observer，最后添加完需要释放掉
+    CFRelease(observer);
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.imageView performSelector:@selector(setImage:) withObject:[UIImage imageNamed:@"icon.png"] afterDelay:2.0 inModes:@[NSDefaultRunLoopMode]];
+    
+    // 利用
+    [self performSelector:@selector(run42) onThread:self.thread withObject:nil waitUntilDone:NO];
+}
+
+- (void)showDemo4 {
+    // 创建线程,并调用run4方法执行任务
+    self.thread = [[NSThread alloc] initWithTarget:self selector:@selector(run41) object:nil];
+    // 开启线程
+    [self.thread start];
+}
+
+- (void)run41 {
+    // 这里写任务
+    NSLog(@"---run41---");
+    
+    // 添加以下两句代码,就可以开启RunLoop,之后self.thread就变成了常驻线程,可以随时添加任务,并交于RunLoop处理
+    [[NSRunLoop currentRunLoop] addPort:[NSPort port] forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop] run];
+    
+    // 测试是否开启了RunLoop,如果开启了RunLoop,则来不了这里,因为RunLoop开启了循环
+    NSLog(@"未开启RunLoop");
+}
+
+- (void)run42 {
+    NSLog(@"---run42---");
+}
 
 @end
