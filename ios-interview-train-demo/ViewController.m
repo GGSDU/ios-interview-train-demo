265fe4ed908e9c29fd71da21868e29ee924a45f5
taochunjie <337995545@qq.com>
2021-03-25 16:14:09 +0800

    3种mode

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -16,6 +16,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+//    [self showDemo1];
+    [self showDemo2];
+}
+
+- (void)showDemo1 {
+    // 定义一个定时器,约定两秒后调用run方法
+    NSTimer *timer = [NSTimer timerWithTimeInterval:2.0 target:self selector:@selector(run) userInfo:nil repeats:YES];
+    // 将定时器添加到当前RunLoop的NSDefaultRunLoopMode下
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:UITrackingRunLoopMode];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+}
+
+- (void)run {
+    NSLog(@"---run");
+}
+
+- (void)showDemo2 {
+    [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(run) userInfo:nil repeats:YES];
+    // 相当于以下两句代码
+//    NSTimer *timer = [NSTimer timerWithTimeInterval:2.0 target:self selector:@selector(run) userInfo:nil repeats:YES];
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
 }
 
 
