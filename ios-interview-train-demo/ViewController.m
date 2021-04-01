87d46e581dc472ba8788b5e532ddd117a97b98cd
taochunjie <337995545@qq.com>
2021-04-01 10:38:07 +0800

    addOperation

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -20,7 +20,8 @@
 //    [self useInvocationOperation];
 //    [self useBlockOperation];
 //    [self useBlockOperationAddExecutionBlock];
-    [self useCustomOperation];
+//    [self useCustomOperation];
+    [self addOperationToQueue];
 }
 
 #pragma mark - NSOperation
@@ -40,7 +41,17 @@
 - (void)task1 {
     for (int i = 0; i < 2; i++) {
         [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
-        NSLog(@"NSInvocationOperation---%@", [NSThread currentThread]); // 打印当前线程
+        NSLog(@"NSInvocationOperation task1---%@", [NSThread currentThread]); // 打印当前线程
+    }
+}
+
+/**
+ * 任务2
+ */
+- (void)task2 {
+    for (int i = 0; i < 2; i++) {
+        [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+        NSLog(@"NSInvocationOperation task2---%@", [NSThread currentThread]); // 打印当前线程
     }
 }
 
@@ -143,5 +154,42 @@
     // 当前队列
     NSOperationQueue *currentQueue = [NSOperationQueue currentQueue];
 }
+
+/**
+ * 使用 addOperation: 将操作加入到操作队列中
+ */
+- (void)addOperationToQueue {
+
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+
+    // 2.创建操作
+    // 使用 NSInvocationOperation 创建操作1
+    NSInvocationOperation *op1 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task1) object:nil];
+
+    // 使用 NSInvocationOperation 创建操作2
+    NSInvocationOperation *op2 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task2) object:nil];
+
+    // 使用 NSBlockOperation 创建操作3
+    NSBlockOperation *op3 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"3---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    [op3 addExecutionBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"4---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+
+    // 3.使用 addOperation: 添加所有操作到队列中
+    [queue addOperation:op1]; // [op1 start]
+    [queue addOperation:op2]; // [op2 start]
+    [queue addOperation:op3]; // [op3 start]
+}
+
+
 
 @end
