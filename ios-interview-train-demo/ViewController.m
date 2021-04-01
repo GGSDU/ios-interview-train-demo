e43bc0003eb3c46b6e712bd27fed7bed6a0a9efa
taochunjie <337995545@qq.com>
2021-04-01 11:01:00 +0800

    mainQueue

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -24,7 +24,8 @@
 //    [self addOperationToQueue];
 //    [self addOperationWithBlockToQueue];
 //    [self setMaxConcurrentOperationCount];
-    [self addDependency];
+//    [self addDependency];
+    [self communication];
 }
 
 #pragma mark - NSOperation
@@ -291,6 +292,33 @@
     // 4.添加操作到队列中
     [queue addOperation:op1];
     [queue addOperation:op2];
+}
+
+/**
+ * 线程间通信
+ */
+- (void)communication {
+
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc]init];
+
+    // 2.添加操作
+    [queue addOperationWithBlock:^{
+        // 异步进行耗时操作
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+
+        // 回到主线程
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // 进行一些 UI 刷新等操作
+            for (int i = 0; i < 2; i++) {
+                [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+                NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+            }
+        }];
+    }];
 }
 
 @end
