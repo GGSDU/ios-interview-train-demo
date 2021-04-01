d58ab2516981f0a1860b495c3281253921e70ecd
taochunjie <337995545@qq.com>
2021-04-01 10:44:29 +0800

    maxConcurrentOperationCount

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -22,7 +22,8 @@
 //    [self useBlockOperationAddExecutionBlock];
 //    [self useCustomOperation];
 //    [self addOperationToQueue];
-    [self addOperationWithBlockToQueue];
+//    [self addOperationWithBlockToQueue];
+    [self setMaxConcurrentOperationCount];
 }
 
 #pragma mark - NSOperation
@@ -216,6 +217,46 @@
         for (int i = 0; i < 2; i++) {
             [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
             NSLog(@"3---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+}
+
+/**
+ * 设置 MaxConcurrentOperationCount（最大并发操作数）
+ */
+- (void)setMaxConcurrentOperationCount {
+
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+
+    // 2.设置最大并发操作数
+    queue.maxConcurrentOperationCount = 1; // 串行队列
+// queue.maxConcurrentOperationCount = 2; // 并发队列
+// queue.maxConcurrentOperationCount = 8; // 并发队列
+
+    // 3.添加操作
+    [queue addOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    [queue addOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    [queue addOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"3---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    [queue addOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"4---%@", [NSThread currentThread]); // 打印当前线程
         }
     }];
 }
