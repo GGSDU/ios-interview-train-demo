5fb8794726e55b50b39e326f0f9569de1cefe81c
taochunjie <337995545@qq.com>
2021-04-01 10:23:48 +0800

    NSInvocationOperation

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -16,7 +16,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self useInvocationOperation];
 }
 
+/**
+ * 使用子类 NSInvocationOperation
+ */
+- (void)useInvocationOperation {
+    // 1.创建 NSInvocationOperation 对象
+    NSInvocationOperation *op = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task1) object:nil];
+    // 2.调用 start 方法开始执行操作
+    [op start];
+}
+
+/**
+ * 任务1
+ */
+- (void)task1 {
+    for (int i = 0; i < 2; i++) {
+        [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+        NSLog(@"NSInvocationOperation---%@", [NSThread currentThread]); // 打印当前线程
+    }
+}
 
 @end
