59c9fc6d788feca402881b99e3e5b4b633e33eff
taochunjie <337995545@qq.com>
2021-04-01 10:55:35 +0800

    addDependency

--- ios-interview-train-demo/ViewController.m
+++ ios-interview-train-demo/ViewController.m
@@ -23,7 +23,8 @@
 //    [self useCustomOperation];
 //    [self addOperationToQueue];
 //    [self addOperationWithBlockToQueue];
-    [self setMaxConcurrentOperationCount];
+//    [self setMaxConcurrentOperationCount];
+    [self addDependency];
 }
 
 #pragma mark - NSOperation
@@ -259,6 +260,37 @@
             NSLog(@"4---%@", [NSThread currentThread]); // 打印当前线程
         }
     }];
+}
+
+/**
+ * 操作依赖
+ * 使用方法：addDependency:
+ */
+- (void)addDependency {
+
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+
+    // 2.创建操作
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+
+    // 3.添加依赖
+    [op2 addDependency:op1]; // 让op2 依赖于 op1，则先执行op1，在执行op2
+
+    // 4.添加操作到队列中
+    [queue addOperation:op1];
+    [queue addOperation:op2];
 }
 
 @end
