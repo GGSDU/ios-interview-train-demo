6dfa8f51359ce15136d639b7c3371d732540c70e
taochunjie <337995545@qq.com>
2021-04-08 10:58:56 +0800

    twoSum

--- ios-interview-train-demo/LeetCode.swift
+++ ios-interview-train-demo/LeetCode.swift
@@ -9,8 +9,33 @@ import Foundation
 
 class LeetCode: NSObject {
     /** 1.两数之和
+     * 暴力枚举
      */
     func twoSum(_ nums:[Int],_ target:Int) -> [Int] {
-        
+//        return twoSumL(nums,target);
+        return twoSumDic(nums, target);
+    }
+    
+    func twoSumL(_ nums:[Int],_ target:Int) -> [Int] {
+        for i in 0...nums.count-1 {
+            for j in i+1...nums.count-1 {
+                if nums[i] + nums[j] == target {
+                    return [i,j]
+                }
+            }
+        }
+        return [];
+    }
+    
+    func twoSumDic(_ nums:[Int],_ target:Int) -> [Int] {
+        var dic = [Int:Int]()
+        for (index,num) in nums.enumerated() {
+            let num2 = target - num
+            if (dic.keys.contains(num2)) {
+                return [dic[num2]!,index]
+            }
+            dic[num] = index;
+        }
+        return []
     }
 }
--- ios-interview-train-demo/ViewController.swift
+++ ios-interview-train-demo/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let leetCode:LeetCode = LeetCode()
+        print(leetCode.twoSum([2,7,11,15], 9))
+        print(leetCode.twoSum([3,2,4], 6))
+        print(leetCode.twoSum([2,5,5,11], 10))
     }
 }
 
